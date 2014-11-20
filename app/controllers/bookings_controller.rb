f1d4dda01556014e6d6129fad9970402985da6f8
MaxMck <mm.moock@gmail.com>
2014-11-20 10:51:41 +0100

    Revert "Revert "Booking""
    This reverts commit 384c9de99d1e9bbcf7379c98ed2c04a6c6056ef7.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,7 @@
 class BookingsController < ApplicationController
+  before_action :set_apartment, only: [:create, :new, :show]
+  before_action :set_booking, only: [:show]
+
   def index
   end
 
@@ -6,17 +9,41 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = @apartment.bookings.new
   end
 
   def create
+    @booking = current_user.bookings.new(booking_params)
+    if @booking.save
+      redirect_to apartment_booking_path(@apartment.id, @booking.id)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+
   end
 
   def destroy
+
   end
+
+ private
+
+  def set_apartment
+    @apartment = Apartment.find(params[:apartment_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
 end
